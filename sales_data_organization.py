8f94d10d8dd0455f9a45db76c41b00ea8e57a34e
sleenonn <rubes246@protonmai.com>
2023-02-01 13:47:40 -0500

    Added 3 Functions
    add_dates - Creates a list of all dates  from the csv and removes the duplicates
    
    order_total - finds the total for each order date and adds a TOTAL PRICE column to the dataframe. I also add a row to the bottom of the dataframe that has a grand total for the order
    
    add_dollar_signs - adds dollar signs to the ITEM PRICE column

--- sales_data_organization.py
+++ sales_data_organization.py
@@ -1,6 +1,8 @@
 
 import os
 import sys # {REQ-1}
+import pandas as pd
+import datetime
 
 def main():
     """Main program function"""
@@ -28,18 +30,71 @@ def valid_input(file_path):
     elif valid_path and valid_path[-4:] == '.csv':
         return file_path
 
-def orders_sub_dirs():
-    """Create sub directorys for certain orders"""
+def all_dates(path_of_csv):
+    """Creates a list of all dates from the files with no dupes""" 
+    # Convert the csv to to a dataframe
+    sales_data_df = pd.read_csv(path_of_csv[1]) 
+    # Normalize the datetime so you can just take the exact date
+    sales_data_df['ORDER DATE'] = pd.to_datetime(sales_data_df['ORDER DATE'])
+    sales_data_df['ORDER DATE'] = sales_data_df['ORDER DATE'].dt.date
+    # Sort the dates inplace from oldest to new
+    sales_data_df.sort_values(by='ORDER DATE', inplace=True)
 
-    return None
+    # Convert the dates to a list 
+    date_list = sales_data_df['ORDER DATE'].tolist()
+    # Store dates in the proper date
+    proper_format = [date.strftime("%Y,%m,%d") for date in date_list]
+
+    # Remove the duplicates
+    no_dupes = []
+    [no_dupes.append(entry) for entry in proper_format if entry not in no_dupes]
+    
+    return no_dupes
+
+
+def order_total(df):
+    """Used to find the total cost of order and grand total"""
+    # Value for the grand total
+    g_total = 0
+    # List to hold the total cost per order
+    total_lst = []
+    for ent in range(len(df)): 
+        # Pull the quantity and variable of the individual item 
+        quantity = df.iloc[ent].at['ITEM QUANTITY']
+        price = df.iloc[ent].at['ITEM PRICE']
+        # Figure out the total price 
+        total_price = (price * quantity) 
+        # Add total price to the grand total 
+        g_total += total_price
+        # Properly format the total string
+        price_formatted = f'${total_price:,}'
+        total_lst.append(price_formatted)
+
+    # Insert all the totals in the new column
+    df.insert(7, 'TOTAL PRICE', total_lst)
+
+    # Add the final row by creating a df and adding it to the
+    # main df.
+    final_row = pd.DataFrame({'ITEM PRICE':['GRAND TOTAL'], 
+        'TOTAL PRICE':[f'${g_total:,}']})
+    df = pd.concat([df, final_row], ignore_index=True, axis=0)
+    # Deal with nan changing other values to floats.
+    df['ORDER ID'] = df['ORDER ID'].astype('Int32')
+    df['ITEM NUMBER'] = df['ITEM NUMBER'].astype('Int32')
+    df['ITEM QUANTITY'] = df['ITEM QUANTITY'].astype('Int32')
+    return df
+
+def add_dollar_signs(df):
+    """Add dollar signs to Item Price"""
+    for ent in range(len(df)):
+        price = df.iloc[ent].at['ITEM PRICE']
+        df.at[ent, 'ITEM PRICE']=f'${price}'
+
+    return df 
 
 
 
 if __name__ == '__main__':
     main()
-    
-
-
-
 
 
